71782fd78cd9724d0bbcb33084adea940381326f
VannesDh <vannesdharmawann@gmail.com>
2026-09-07 09:44:59 +0700

    Added additional fluent api

--- BasicCrud/Data/db.cs
+++ BasicCrud/Data/db.cs
@@ -4,23 +4,36 @@ namespace BasicCrud.Data;
 
 public class AppDbContext : DbContext
 {
-     public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options)
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+       : base(options)
     {
     }
 
-    public DbSet<Restaurant> Restaurants {get; set;}
-    public DbSet<Food> Foods {get; set;}
+    public DbSet<Restaurant> Restaurants { get; set; }
+    public DbSet<Food> Foods { get; set; }
 
-   protected override void OnModelCreating(ModelBuilder modelBuilder)
-{
-    modelBuilder.Entity<Restaurant>(entity =>
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        entity.HasKey(r => r.Id);
-        
-        entity.Property(r => r.RestaurantType)
-            .HasConversion<string>();
-    });
-}
-    
+        modelBuilder.Entity<Restaurant>(entity =>
+        {
+            entity.HasKey(r => r.Id);
+
+            entity.Property(r => r.Name)
+             .IsRequired()
+             .HasMaxLength(100);
+
+            entity.Property(r => r.RestaurantType)
+                .HasConversion<string>();
+        });
+
+        modelBuilder.Entity<Food>(entity =>
+        {
+            entity.HasKey(f => f.Id);
+
+            entity.Property(f => f.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+        });
+    }
+
 }
